1658da45ec70e9b9cd3d61d9a63b0c07c5e83d73
박병용 <qkrquddyd94@gmail.com>
2019-03-24 18:27:07 +0900

    [파일 추가] Comic 데이터 보여줄 RecyclerView의 Adapter

---
+++ app/src/main/java/com/pby/androidfirebasecomicreader/Adapter/MyComicAdapter.java
@@ -0,0 +1,59 @@
+package com.pby.androidfirebasecomicreader.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pby.androidfirebasecomicreader.Model.Comic;
+import com.pby.androidfirebasecomicreader.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class MyComicAdapter extends RecyclerView.Adapter<MyComicAdapter.MyViewHolder> {
+
+    Context context;
+    List<Comic> comicList;
+    LayoutInflater inflater;
+
+    public MyComicAdapter(Context context, List<Comic> comicList) {
+        this.context = context;
+        this.comicList = comicList;
+        inflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public MyComicAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = inflater.inflate(R.layout.item_comic, viewGroup, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyComicAdapter.MyViewHolder myViewHolder, int i) {
+        Picasso.get().load(comicList.get(i).Image).into(myViewHolder.imageComic);
+        myViewHolder.textComicName.setText(comicList.get(i).Name);
+    }
+
+    @Override
+    public int getItemCount() {
+        return comicList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private ImageView imageComic;
+        private TextView textComicName;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageComic = (ImageView) itemView.findViewById(R.id.image_comic);
+            textComicName = (TextView) itemView.findViewById(R.id.text_comic_name);
+        }
+    }
+}
