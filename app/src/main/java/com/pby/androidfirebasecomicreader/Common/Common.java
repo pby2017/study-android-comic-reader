5da59dad050dc91b7736e0889b44343440e3fd62
박병용 <qkrquddyd94@gmail.com>
2019-03-24 18:27:50 +0900

    [파일 추가] comicList를 따로 저장함. 왜 추가했는지 모르겠지만, 나중에 global 하게 사용할 것 같음.

---
+++ app/src/main/java/com/pby/androidfirebasecomicreader/Common/Common.java
@@ -0,0 +1,10 @@
+package com.pby.androidfirebasecomicreader.Common;
+
+import com.pby.androidfirebasecomicreader.Model.Comic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Common {
+    public static List<Comic> comicList = new ArrayList<>();
+}
